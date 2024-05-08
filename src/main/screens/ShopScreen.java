9ac840b6e2d343392058819cb76c7032686c2d36
wim <wim@gbor.nl>
2024-05-08 14:23:19 +0200

    plaatjes toegevoegd

--- src/main/screens/ShopScreen.java
+++ src/main/screens/ShopScreen.java
@@ -1,5 +1,6 @@
 package main.screens;
 
+import main.GamePanel;
 import main.StaticImage.StaticImage;
 
 import java.awt.event.MouseEvent;
@@ -10,6 +11,21 @@ public class ShopScreen extends Screen {
     }
 
     private void addEntities(){
+
+        StaticImage kast = new StaticImage("sprites/kastgroot.png", 0 , 0);
+        int kastX = (GamePanel.screenWidth / 2) - (kast.getWidth() / 2);
+        int kastY = GamePanel.screenHeight - (kast.getHeight() + 400);
+        kast.setX(kastX);
+        kast.setY(kastY);
+        this.drawables.add(kast);
+
+        StaticImage cauldron = new StaticImage("sprites/cauldrongroot.png", 0,0);
+        int startKnopX = (GamePanel.screenWidth / 2) - (cauldron.getWidth() / 2);
+        int startKnopY = GamePanel.screenHeight - (cauldron.getHeight() + 100);
+        cauldron.setX(startKnopX);
+        cauldron.setY(startKnopY);
+        this.drawables.add(cauldron);
+
         StaticImage testSprite = new StaticImage("sprites/witch.jpeg", 0, 0);
         this.drawables.add(testSprite);
     }
